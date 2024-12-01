8a6c961aec2e35d7152823e500088796b42e8950
Sushma Gowda <153488285+Sushmaa123@users.noreply.github.com>
2024-12-01 17:32:41 +0530

    Update index.js

--- registration-server-Badhaan/index.js
+++ registration-server-Badhaan/index.js
@@ -1,28 +1,18 @@
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
+const express=require("express");
+const app=express();
+const mongoose=require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
-
-const port = process.env.PORT || 5000;
-const userRouter = require("./Routes/user");
-
-app.use("/", userRouter);
-
-app.get("/", (req, res) => {
-    res.send("hello world");
-});
-
-app.listen(port, () => {
-    mongoose.connect('mongodb://mongo:27017/budhan-users', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        bufferTimeoutMS: 30000,       // Sets buffer timeout to 30 seconds
-        connectTimeoutMS: 30000,      // Sets connection timeout to 30 seconds
-        socketTimeoutMS: 45000        // Sets socket timeout to 45 seconds
-    }, () => {
-        console.log("connected to database");
-    });
-
+const port=5000 || process.env.PORT;
+const userRouter=require("./Routes/user");
+app.use("/",userRouter);
+app.get("/",(req,res)=>{
+    res.send("hello world")
+})
+app.listen(port,()=>{
+   mongoose.connect('mongodb://mongo:27017/budhan-users',{useNewUrlParser: true},()=>{
+//    mongoose.connect('mongodb://localhost:27017/budhan-users',{useNewUrlParser: true},()=>{
+    console.log("connected to database");
+})
     console.log(`server is up at ${port}`);
-});
+})
